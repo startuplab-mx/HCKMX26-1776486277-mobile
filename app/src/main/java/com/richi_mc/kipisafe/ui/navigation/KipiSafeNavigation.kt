66958e1973b81aff3723c77e5e4a16178a7cc334
Mendoza Castañeda José Ricardo <mendozacastanedajosericardo@gmail.com>
2026-04-24 23:47:51 -0600

    Navegación de la app integrada

---
+++ app/src/main/java/com/richi_mc/kipisafe/ui/navigation/KipiSafeNavigation.kt
@@ -0,0 +1,104 @@
+package com.richi_mc.kipisafe.ui.navigation
+
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BarChart
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.richi_mc.kipisafe.ui.presentation.home.HomeScreen
+import com.richi_mc.kipisafe.ui.presentation.metrics.MetricsScreen
+
+data class NavigationItem<T : Any>(
+    val title: String,
+    val route: T,
+    val icon: ImageVector
+)
+
+@Composable
+fun KipiSafeNavigation() {
+    val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    val navigationItems = listOf(
+        NavigationItem("Inicio", Home, Icons.Default.Home),
+        NavigationItem("Estadísticas", Stats, Icons.Default.BarChart),
+        NavigationItem("Ajustes", Settings, Icons.Default.Settings)
+    )
+
+    Scaffold(
+        contentWindowInsets = WindowInsets(top = 0.dp),
+        bottomBar = {
+            NavigationBar {
+                navigationItems.forEach { item ->
+
+                    val isSelected = currentDestination?.hasRoute(item.route::class) == true
+
+                    NavigationBarItem(
+                        selected = isSelected,
+                        onClick = {
+                            navController.navigate(item.route) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        },
+                        icon = {
+                            Icon(
+                                imageVector = item.icon,
+                                contentDescription = item.title
+                            )
+                        },
+                        label = {
+                            Text(
+                                text = item.title,
+                                style = MaterialTheme.typography.bodyMedium,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Home,
+            modifier = Modifier
+                .padding(innerPadding)
+        ) {
+            composable<Home> {
+                HomeScreen()
+            }
+
+            composable<Stats> {
+                MetricsScreen()
+            }
+
+            composable<Settings> {
+                // TODO: Implementar pantalla de Ajustes
+            }
+        }
+    }
+}
